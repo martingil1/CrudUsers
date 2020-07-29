a48011c1bbc6ba3f92659f7725f4c759d8a101ba
martingil1 <martinfacundogil@gmail.com>
2020-07-28 23:11:27 -0300

    [Create package models] create model User and Administrator

---
+++ src/main/java/com/crud/login/models/Administrator.java
@@ -0,0 +1,50 @@
+package com.crud.login.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Administrators")
+public class Administrator {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @com.sun.istack.NotNull
+    @Column(name = "name")
+    private String name;
+
+    @com.sun.istack.NotNull
+    @Column(name = "last_name")
+    private String lastName;
+
+    @com.sun.istack.NotNull
+    @Column(name = "admin_code")
+    private String adminCode;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
